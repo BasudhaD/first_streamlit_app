ffd4f107c6506486f5d0c51e0794521301d3341c
BasudhaD <131553185+BasudhaD@users.noreply.github.com>
2023-04-23 00:58:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
-streamlit.title('Learning Snowflake is very interesting ')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blue berry Oat meal')

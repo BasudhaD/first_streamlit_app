d91d5618709c4152d364225c39e85b49dd0a03a1
BasudhaD <131553185+BasudhaD@users.noreply.github.com>
2023-04-27 02:11:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ except URLError as e:
       streamlit.error()
 
 
-
+streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
           my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
@@ -62,7 +62,7 @@ streamlit.stop()
 
 
 
-streamlit.header("The fruit load list contains:")
+
 
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
